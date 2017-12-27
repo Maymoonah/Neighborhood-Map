3d3272e5ac7410665b56b158debb645b47863cc7
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2017-12-27 13:07:11 -0800

    add info window to all markers

--- js/app.js
+++ js/app.js
@@ -11,20 +11,32 @@ function initMap() {
 
 	//creating markers
 	let markers = [
-		{lat: 34.020174, lng: -117.864982},
-		{lat: 34.047693, lng: -117.844858},
-		{lat: 33.994873, lng: -117.884711},
-		{lat: 34.027874, lng: -117.833771},
-		{lat: 34.035642, lng: -117.805326}
+		{title: 'Donut Tree', location: {lat: 34.020174, lng: -117.864982}},
+		{title: 'Mt. San Antonio College', location: {lat: 34.047693, lng: -117.844858}},
+		{title: 'ICSGV', location: {lat: 33.994873, lng: -117.884711}},
+		{title: 'Yogurtland', location: {lat: 34.027874, lng: -117.833771}},
+		{title: 'Oak Tree Lanes', location: {lat: 34.035642, lng: -117.805326}}
 	];
 
 	//adding markers to map
 	for(let i = 0; i < markers.length; i++) {
-		let	marker = new google.maps.Marker({
-			position: markers[i],
+		let marker = new google.maps.Marker({
+			position: markers[i].location,
 			map: map,
+			title: markers[i].title,
 			animation: google.maps.Animation.DROP,
-			id: i
+		});
+
+		//create info windows
+		let infowindow = new google.maps.InfoWindow({
+			content: markers[i].title
+		});
+
+		//listen for click on markers
+		marker.addListener('click', function() {
+			infowindow.open(map, marker);
 		});
 	}
 }
+
+
