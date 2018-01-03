7b54b167d5ed33022fef10a62e00aab2bec4f791
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-01-03 01:03:30 -0800

    add icon images to markers array and addMarkers function

--- js/app.js
+++ js/app.js
@@ -13,18 +13,20 @@ function initMap() {
 	//View Model
 	ViewModel = {
 		markers: ko.observableArray([
-			{title: 'Donut Tree', location: {lat: 34.020174, lng: -117.864982}},
-			{title: 'Mt. San Antonio College', location: {lat: 34.047693, lng: -117.844858}},
-			{title: 'ICSGV', location: {lat: 33.994873, lng: -117.884711}},
-			{title: 'Yogurtland', location: {lat: 34.027874, lng: -117.833771}},
-			{title: 'Oak Tree Lanes', location: {lat: 34.035642, lng: -117.805326}},
-			{title: 'Nogales High School', location: {lat: 34.009556, lng: -117.889152}},			
-			{title: 'Pizza Hut', location: {lat: 34.011620, lng: -117.886432}},
-			{title: 'Domino\'s Pizza', location: {lat: 34.027549, lng: -117.893225}},
-			{title: 'Baskin-Robbins', location: {lat: 34.011885, lng: -117.886430}},
-			{title: 'Suzanne Middle School', location: {lat: 34.025070, lng: -117.850407}},
-			{title: 'Walnut High School', location: {lat: 34.021202, lng: -117.849043}},
-			{title: 'Walnut High School', location: {lat: 34.021202, lng: -117.849043}}
+			{title: 'Krispy Kreme', location: {lat: 33.994923, lng: -117.930837}, icon: './images/donut.png'},
+			{title: 'Mt. San Antonio College', location: {lat: 34.047693, lng: -117.844858}, icon: './images/college.png'},
+			{title: 'ICSGV', location: {lat: 33.994873, lng: -117.884711}, icon: './images/mosque.png'},
+			{title: 'Yogurtland', location: {lat: 34.027874, lng: -117.833771}, icon: './images/frozenyogurt.png'},
+			{title: 'Oak Tree Lanes', location: {lat: 34.035642, lng: -117.805326}, icon: './images/bowling.png'},
+			{title: 'Nogales High School', location: {lat: 34.009556, lng: -117.889152}, icon: './images/highschool.png'},			
+			{title: 'Pizza Hut', location: {lat: 34.011620, lng: -117.886432} ,icon: './images/pizza.png'},
+			{title: 'Domino\'s Pizza', location: {lat: 34.027549, lng: -117.893225}, icon: './images/pizza.png'},
+			{title: 'Baskin-Robbins', location: {lat: 34.011885, lng: -117.886430}, icon: './images/icecream.png'},
+			{title: 'Suzanne Middle School', location: {lat: 34.025070, lng: -117.850407}, icon: './images/school.png'},
+			{title: 'Walnut High School', location: {lat: 34.021202, lng: -117.849043}, icon: './images/highschool.png'},
+			{title: 'Farmers Market', location: {lat: 34.020304, lng: -117.811443}, icon: './images/farmersmarket.png'},
+			{title: 'South Pointe Middle School', location: {lat: 33.990842, lng: -117.848859}, icon: './images/school.png'},
+			{title: 'Los Angeles County Fire Dept.', location: {lat: 34.020786, lng: -117.865621}, icon: './images/firetruck.png'}			
 		]),
 
 		addMarker: function() {
@@ -33,6 +35,7 @@ function initMap() {
 					position: this.markers()[i].location,
 					map: map,
 					title: this.markers()[i].title,
+					icon: this.markers()[i].icon,
 					animation: google.maps.Animation.DROP
 				});
 				
