149d4eefc4ff881fb3557fb9840581f78bb15b57
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-01-16 00:27:11 -0800

    changing ViewModel from object literal to constructor function

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), options);
 
 	//View Model
-	ViewModel = {
-		markers: ko.observableArray([
+	ViewModel = function() {
+		this.markers = ko.observableArray([
 			{title: 'Krispy Kreme', location: {lat: 33.994923, lng: -117.930837}, icon: './images/donut.png'},
 			{title: 'Mt. San Antonio College', location: {lat: 34.047693, lng: -117.844858}, icon: './images/college.png'},
 			{title: 'Islamic Center of San Gabriel Valley', location: {lat: 33.994873, lng: -117.884711}, icon: './images/mosque.png'},
@@ -31,10 +31,10 @@ function initMap() {
 			{title: 'Stop Break Shops', location: {lat: 33.998230, lng: -117.884749}, icon: './images/mechanic.png'},			
 			{title: 'Chevron', location: {lat: 34.015822, lng: -117.850391}, icon: './images/gas-station.png'},			
 			{title: 'Bank of America', location: {lat: 34.013161, lng: -117.861232}, icon: './images/bank.png'}
-		]),
+		]);
 
 		//add marker to map
-		addMarker: function() {
+		this.addMarker = function() {
 			let self = this;
 			for(let i = 0; i < self.markers().length; i++) {
 				marker = new google.maps.Marker({
@@ -47,10 +47,10 @@ function initMap() {
 				self.addInfoWindow();
 				self.callAPI();
 			}
-		},
+		}
 
 		//add infowindow to marker
-		addInfoWindow: function() {
+		this.addInfoWindow = function() {
 			let self = this;
 			//create info windows
 			infowindow = new google.maps.InfoWindow({maxWidth: 200});
@@ -72,10 +72,10 @@ function initMap() {
 		            <a href=${wikiUrl}>${siteUrl}</a></br>
 		            <img src=${pic}>`);
 			});			
-		},
+		}
 
 		//call wikipedia and Flickr APIs
-		callAPI: function() {
+		this.callAPI = function() {
 			//Wikipedia AJAK request from Udacity Intro to AJAX with some adjustments
 		    wikiUrl = `http://en.wikipedia.org/w/api.php?action=opensearch&search=${marker.title}&format=json&callback=wikiCallback`;
 		    //if page takes long time to load
@@ -106,29 +106,26 @@ function initMap() {
 					pic = item.media.m;
 				});
 			});
-		},
+		}
 
 		// filter the items using the filter text
-		filterText: ko.observable(''),
-		filteredLoc: ko.computed(function() {
+		this.filterText = ko.observable('');
+		this.filteredLoc = ko.computed(function() {
 			let self = this;
-			if (!self.filterText) {
+			if (!this.filterText) {
 		        return self.markers;
-		        console.log(filterText);
 		    } else {
-				return ko.utils.arrayFilter(self.markers, function(item) {
-					return ko.utils.stringStartsWith(item.title.toLowerCase(), self.filterText());
+				return ko.utils.arrayFilter(this.markers, function(item) {
+					return ko.utils.stringStartsWith(item.title.toLowerCase(), this.filterText());
 					console.log(filterText);
 				});
 			}		
-		})
+		});
+		//call addMarkers
+		this.addMarker();
+	}
 
-
-	};
-	
 	//apply bindings and sort list
-	ko.applyBindings(ViewModel);
-
-	//call addMarkers
-	ViewModel.addMarker();	
+	ko.applyBindings(new ViewModel());	
+	
 }
